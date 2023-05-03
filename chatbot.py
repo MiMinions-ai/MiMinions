e32b28eaf3f063cbfbb8855a096dc9a3841c0fe5
Roland Ding <shengxio@lanterns.fun>
2023-05-02 20:04:54 -0600

    v0.0.1 a miniature openai chatgpt system.

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+import gradio as gr
+from supplier import *
+
+background_examples=[
+    "This is a friendly chatbot with humour.", # casual setting
+    "This is the first date between virtual human and real human.", # romantic setting
+    "We are at the mideval castle, I am talking to my chatbot donkey.", # fantasy setting
+]
+
+with gr.Blocks() as chatbot_iface:
+    with gr.Column():
+        with gr.Row():
+            background = gr.TextArea(sys_msg[0]["content"],lines=5,label="Current background")
+            with gr.Column():
+                def update_sys(text):
+                    sys_msg[0].update({"content":text})
+                    return text
+                
+                background_edit = gr.Textbox(placeholder="change the background here",label="update background",lines = 5)
+                background_edit.submit(update_sys,background_edit,background)
+                with gr.Row():
+                    clear = gr.Button("Clear")
+                    submit = gr.Button("Submit")
+
+                gr.Examples(background_examples,background_edit,label="Examples")
+                clear.click(lambda:None,None,background_edit,queue=False)
+                submit.click(update_sys,background_edit,background,queue=False)
+
+    with gr.Column():
+        chatbot = gr.Chatbot(initial_msg)
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+
+    msg.submit(send_chat,[msg,chatbot],[msg,chatbot])
+    clear.click(lambda:None,None,chatbot,queue=False)
+
+if __name__ == "__main__":
+    chatbot_iface.launch()
---
+++ supplier.py
@@ -0,0 +1,58 @@
+import openai
+import json
+import os
+
+
+# with open("openai_api_key.json", "r") as f:
+#     openai_api_key = json.load(f)["key"]
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+sys_msg = [{
+    "role": "system",
+    "content": "Hi, I'm a chatbot! Ask me a question."
+}]
+initial_msg = []
+
+def send_chat(text,messages=initial_msg):
+    '''
+    send_chat function takes two arguments: text and messages
+
+    text: the text that the user inputs
+    messages: a list of tuples, each tuple contains two strings, the first string is the user input, the second string is the assistant output
+
+    return: a tuple, the first element is an empty string, the second element is the updated messages list
+
+    '''
+    openai_messages = sys_msg.copy()
+
+    for m in messages:
+        openai_messages.extend([
+            {
+                "role": "user",
+                "content": m[0]
+            },
+            {
+                "role": "assistant",
+                "content": m[1]
+            }
+        ])
+
+    openai_messages.extend([
+        {
+            "role": "user",
+            "content": text
+        }
+    ])
+
+    res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=openai_messages
+        )
+    
+    assistant_text = res.choices[0]["message"]["content"]
+    
+    messages.append((text,assistant_text))
+
+    return "",messages
