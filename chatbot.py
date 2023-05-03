c897a3f7784896cd305e75ec9bd0533ba3a2e21f
Roland Ding <shengxio@lanterns.fun>
2023-05-02 23:46:21 -0600

    v0.0.4 updated the chat window and settings into different tabs

--- chatbot.py
+++ chatbot.py
@@ -7,25 +7,25 @@ background_examples=[
     "We are at the mideval castle, I am talking to my chatbot donkey.", # fantasy setting
 ]
 
-with gr.Blocks() as chatbot_iface:
-    with gr.Column():
-        with gr.Row():
-            background = gr.TextArea(sys_msg[0]["content"],lines=5,label="Current background")
-            with gr.Column():
-                def update_sys(text):
-                    sys_msg[0].update({"content":text})
-                    return text
-                
-                background_edit = gr.Textbox(placeholder="change the background here",label="update background",lines = 5)
-                background_edit.submit(update_sys,background_edit,background)
-                with gr.Row():
-                    clear = gr.Button("Clear")
-                    submit = gr.Button("Submit")
+with gr.Blocks() as settings:
+    with gr.Row():
+        background = gr.TextArea(sys_msg[0]["content"],lines=5,label="Current background")
+        with gr.Column():
+            def update_sys(text):
+                sys_msg[0].update({"content":text})
+                return text
+            
+            background_edit = gr.Textbox(placeholder="change the background here",label="update background",lines = 5)
+            background_edit.submit(update_sys,background_edit,background)
+            with gr.Row():
+                clear = gr.Button("Clear")
+                submit = gr.Button("Submit")
 
-                gr.Examples(background_examples,background_edit,label="Examples")
-                clear.click(lambda:None,None,background_edit,queue=False)
-                submit.click(update_sys,background_edit,background,queue=False)
-
+            gr.Examples(background_examples,background_edit,label="Examples")
+            clear.click(lambda:None,None,background_edit,queue=False)
+            submit.click(update_sys,background_edit,background,queue=False)
+            
+with gr.Blocks() as chat_window:
     with gr.Column():
         chatbot = gr.Chatbot(initial_msg)
         msg = gr.Textbox()
@@ -34,5 +34,7 @@ with gr.Blocks() as chatbot_iface:
     msg.submit(send_chat,[msg,chatbot],[msg,chatbot])
     clear.click(lambda:None,None,chatbot,queue=False)
 
+iface = gr.TabbedInterface([chat_window,settings],[["Chatbot","Settings"]],"bottom",title="Chatbot",description="A chatbot with GPT-3.5 Turbo")
+
 if __name__ == "__main__":
-    chatbot_iface.launch(server_name="0.0.0.0")
+    iface.launch(server_name="0.0.0.0")
