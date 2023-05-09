522cb46397c819d6d20c9b29f4bf52da60f987f4
Roland Ding <shengxio@lanterns.fun>
2023-05-09 10:57:37 -0600

    v0.1.7 revised server port.

--- chatbot.py
+++ chatbot.py
@@ -56,4 +56,5 @@ if __name__ == "__main__":
         server_name="0.0.0.0",
         ssl_verify=False,
         ssl_certfile="./cert.pem",
-        ssl_keyfile="./key.pem")
+        ssl_keyfile="./key.pem",
+        server_port=80)
