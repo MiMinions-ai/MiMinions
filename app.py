d415e7e4f87036a872a461adafe735cabfad3c31
Roland Ding <shengxio@lanterns.fun>
2023-07-02 22:34:36 -0600

    0.0.2.2 double check.

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ from supplier import *
 
 background_examples=[
     "This is a friendly chatbot with humour.", # casual setting
-    "This is the first date between virtual human and real human.", # romantic setting
+    "This is a friendly chatbot which respond to user in Chinese.", # romantic setting
     "We are at the mideval castle, I am talking to my chatbot donkey.", # fantasy setting
 ]
 
+# create a system setting for llm
 with gr.Blocks() as settings:
     with gr.Row():
-        background = gr.TextArea(sys_msg[0]["content"],lines=5,label="Current background")
+        background = gr.TextArea(App_state["sys_msg"][0]["content"],lines=5,label="Current background")
         with gr.Column():
             def update_sys(text):
-                sys_msg[0].update({"content":text})
+                App_state["sys_msg"][0].update({"content":text})
                 return text
             
             background_edit = gr.Textbox(placeholder="change the background here",label="update background",lines = 5)
@@ -24,31 +25,39 @@ with gr.Blocks() as settings:
             gr.Examples(background_examples,background_edit,label="Examples")
             clear.click(lambda:None,None,background_edit,queue=False)
             submit.click(update_sys,background_edit,background,queue=False)
+    with gr.Row():
+        voices_list = gr.Dropdown([v["Name"] for v in get_voices()],label="Voices")
+        
+    voices_list.change(lambda voice:App_state.update({"voice":voice}),voices_list,queue=False)
             
 with gr.Blocks() as chat_window:
     with gr.Row():
         with gr.Column():
-            chatbot = gr.Chatbot(initial_msg)
-            clear = gr.Button("Clear")
-        
+            chatbot = gr.Chatbot([])
+            chatbot_speech = gr.Audio()
+            with gr.Column():
+                chat_clear = gr.Button("Clear")
+                play_speech = gr.Button("Play")
+        chat_clear.click(lambda:None,None,chatbot,queue=False)
+        play_speech.click(text_to_audio,chatbot,chatbot_speech,queue=False)
+
         with gr.Column():
             msg = gr.Textbox()
             submit = gr.Button("Submit")
             gr.Examples(["Hello","How are you?"],msg,label="Examples")
-                
             audio = gr.Audio(source="microphone",type="filepath")
             # gr.Interface(translate,inputs=gr.Audio(source="microphone",type="filepath"),outputs = "text")
 
-    audio.change(translate,audio,msg,queue=False)
-    msg.submit(send_chat,[msg,chatbot],[msg,chatbot])
-    submit.click(send_chat,[msg,chatbot],[msg,chatbot],queue=False)
+        audio.change(translate,audio,msg,queue=False)
+        msg.submit(send_chat,[msg,chatbot],[msg,chatbot])
+        submit.click(send_chat,[msg,chatbot],[msg,chatbot],queue=False)
 
-    clear.click(lambda:None,None,chatbot,queue=False)
+
 
 iface = gr.TabbedInterface(
     [chat_window,settings],
     ["Chat","Settings"],
-    title="ChatGPT 3.5",
+    title="MiMinions.ai",
     css="footer {visibility: hidden}")
 
 if __name__ == "__main__":
--- supplier.py
+++ supplier.py
@@ -1,15 +1,56 @@
 import openai
+import boto3
 import os
 
+import pydub
+import numpy as np
+
+# setup the api keys
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 sys_msg = [{
     "role": "system",
     "content": "Hi, I'm a chatbot! Ask me a question."
 }]
-initial_msg = []
 
-def send_chat(text,messages=initial_msg):
+App_state = {
+    "messages":[],
+    "sys_msg":[{
+    "role": "system",
+    "content": "Hi, I'm a chatbot! Ask me a question."
+}],
+    "language":"en",
+    # "last_msg":"",
+    "voice":"Joanna",
+}
+
+# setup the polly client
+polly_client = boto3.client(
+    "polly", # the service we want to use
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name='us-east-1')
+
+def get_voices():
+    return polly_client.describe_voices()["Voices"]
+
+# return the audio based on the text
+def text_to_audio(messages,voice="Joanna",sample_rate = 22050):
+    text = messages[-1][1]
+    response = polly_client.synthesize_speech(VoiceId=voice,
+                OutputFormat='mp3',
+                SampleRate=str(sample_rate),
+                Text = text)
+    
+    buffer = response["AudioStream"].read()
+    audio_stream = np.frombuffer(buffer,dtype=np.uint16)
+    
+
+    return sample_rate,audio_stream
+
+def send_chat(text,messages=[]):
     '''
     send_chat function takes two arguments: text and messages
 
@@ -19,7 +60,7 @@ def send_chat(text,messages=initial_msg):
     return: a tuple, the first element is an empty string, the second element is the updated messages list
 
     '''
-    openai_messages = sys_msg.copy()
+    openai_messages = App_state["sys_msg"]
 
     for m in messages:
         openai_messages.extend([
@@ -48,6 +89,7 @@ def send_chat(text,messages=initial_msg):
     assistant_text = res.choices[0]["message"]["content"]
     
     messages.append((text,assistant_text))
+    App_state.update({"messages":messages})
 
     return "",messages
 
